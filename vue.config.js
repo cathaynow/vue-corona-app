b28fb960dc8760bf76e9795d12b3333290cc46f7
CATHAY <dodeux81@gmail.com>
2022-10-15 13:22:14 +0900

    deploy retry

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
 });
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "productgion" ? "/vue-corona-app" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-corona-app" : "/",
   outputDir: "docs",
 };
